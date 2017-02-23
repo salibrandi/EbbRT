9ea9eb7c1ef76c3e5e79a986d5a9478b03721c9b
Stephanie Alibrandi <smaf91@bu.edu>
2017-02-23 11:48:15 -0500

    perf(PoolAllocator): Round robin spawn though available cpus

--- src/hosted/PoolAllocator.cc
+++ src/hosted/PoolAllocator.cc
@@ -12,6 +12,7 @@
 
 //#include "../CapnpMessage.h"
 #include "Debug.h"
+#include "Cpu.h"
 #include "Messenger.h"
 #include "PoolAllocator.h"
 #include "NodeAllocator.h"
@@ -27,20 +28,27 @@ void ebbrt::PoolAllocator::AllocateNode() {
       ebbrt::node_allocator->AllocateNode(binary_path_)
   );
   std::cerr << "Allocated Node: " << num_nodes_ << std::endl;
+  // Node ready to work, set future to true
   pool_futures[num_nodes_].SetValue(1);
   num_nodes_++;
 }
 
 void ebbrt::PoolAllocator::AllocatePool(std::string binary_path, 
     int num_nodes) {
+
   num_nodes_ = 0;
   binary_path_ = binary_path;
   std::cerr << "Pool Allocation Details: " << std::endl;
   std::cerr << "|   img: " << binary_path_ << std::endl;
   std::cerr << "| nodes: " << num_nodes_ << std::endl;
+
+  int cpu_num = ebbrt::Cpu::GetPhysCpus();
+  // Round robin through the available cpus
   for (int i = 0; i < num_nodes; i++) {
-    ebbrt::event_manager->Spawn([this]() {
+    auto cpu_i = ebbrt::Cpu::GetByIndex(i % cpu_num);
+    auto ctxt = cpu_i->get_context();
+    ebbrt::event_manager->SpawnRemote([this]() {
         AllocateNode();
-    }, true);
+    }, ctxt);
   }
 }
