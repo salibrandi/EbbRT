eeaa79e74c616135e48330836920d251e572fd67
Stephanie Alibrandi <smaf91@bu.edu>
2017-02-23 11:50:21 -0500

    feat(Printer): Send message to backend to  execute task
    Front end waits for back end futures to be fulfilled
    before sending Hello World message.

--- apps/helloworld-pool/src/hosted/helloworld.cc
+++ apps/helloworld-pool/src/hosted/helloworld.cc
@@ -4,8 +4,6 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #include <iostream>
-// TODO: remember to delete this after testing
-#include <thread>
 #include <chrono>
 #include <signal.h>
 
@@ -17,36 +15,49 @@
 #include "Printer.h"
 
 static char* ExecName = 0;
+static unsigned int NumNodes = 2;
 
 void AppMain() {
   auto bindir = boost::filesystem::system_complete(ExecName).parent_path() /
     "/bm/helloworld.elf32";
+  unsigned int num_nodes = NumNodes;
 
-  Printer::Init().Then([bindir](ebbrt::Future<void> f) {
-      f.Get();
-      try {
-      ebbrt::pool_allocator->AllocatePool(bindir.string(), 5);
-      } catch (std::runtime_error& e) {
+  Printer::Init().Then([bindir, num_nodes](ebbrt::Future<void> f) {
+    f.Get();
+    try {
+      ebbrt::pool_allocator->AllocatePool(bindir.string(), num_nodes);
+    } catch (std::runtime_error& e) {
       std::cout << e.what() << std::endl;
       exit(1);
-      }
-      });
+    }
+  });
 
-
-  for (unsigned int i=0 ; i < ebbrt::pool_allocator->node_descriptors_.size(); i++) {
-    ebbrt::pool_allocator->node_descriptors_[i].NetworkId().Then([](ebbrt::Future<ebbrt::Messenger::NetworkId> inner) {
-        auto nid = inner.Get();
-        printer->Execute(nid);
-        });
+  // Initialize Pool futures
+  for (unsigned int i=0 ; i < num_nodes; i++) {
+    ebbrt::pool_allocator->pool_futures.push_back(ebbrt::Promise<int>());
   }
+
+  for (unsigned int i=0 ; i < num_nodes; i++) {
+    // Waiting for Future
+    auto nf = ebbrt::pool_allocator->pool_futures[i].GetFuture();
+    nf.Block();
+    ebbrt::pool_allocator->node_descriptors_[i].NetworkId().Then([](ebbrt::Future<ebbrt::Messenger::NetworkId> inner) {
+      auto nid = inner.Get();
+      printer->Execute(nid);
+    });
+  }
+
 }
 
 int main(int argc, char** argv) {
   void* status;
 
   ExecName = argv[0];
+  if (argc > 1) {
+    NumNodes = std::atoi(argv[1]);
+  }
 
-  pthread_t tid = ebbrt::Cpu::EarlyInit(1);
+  pthread_t tid = ebbrt::Cpu::EarlyInit(5);
 
   pthread_join(tid, &status);
 
